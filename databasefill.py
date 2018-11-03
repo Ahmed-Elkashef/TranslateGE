0abe8020a629ee411739217d4a494babbbcc22de
Ahmed Elkashef <ahmedelkashef2012@gmail.com>
2018-11-03 23:27:36 +0200

    hide elements on focus of input

--- databasefill.py
+++ databasefill.py
@@ -29,4 +29,24 @@ Katze = Word(vocabulary_word='katze', short_description="What's another name for
 session.add(Katze)
 session.commit()
 
+Katalog = Word(vocabulary_word='katalog', short_description="A catalog is a book that lists many things: the"
++" most common type of catalog is for a store.", long_description="A catalog is an organized list that"
++" appears in book or pamphlet form. The Sears catalog tells you all the things you can buy at Sears,"
++" along with pictures of the items and what they cost. But you can also use catalog to mean any kind"
++" of listing. In an argument, you might say, 'Why don't you just make a catalog of all my faults!' "
++"When you're making a catalog of any kind, you're cataloging.")
+session.add(Katalog)
+session.commit()
+
+Konkret = Word(vocabulary_word='konkret', short_description="Concrete is that pourable mix of cement,"
++" water, sand, and gravel that hardens into a super-strong building material. Sidewalks, foundations,"
++" and highways are all made of concrete.", long_description="Though people use the words cement and"
++" concrete as if they were the same, they're not. Concrete has cement in it, but also includes other"
++" materials; cement is what binds concrete together. Construction workers hate when kids write their"
++" names in concrete before it hardens: once concrete hardens, it's going to be solid for a long time."
++" When used as an adjective, concrete also means solid. If you've got concrete plans for Saturday,"
++" then you have a definite plan.")
+session.add(Konkret)
+session.commit()
+
 print("added words!")
